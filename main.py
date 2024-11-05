d4ee2b28693936064b568a25d05a3e1affc6c253
vellimir <kkazancov@gmail.com>
2024-11-05 23:03:08 +0400

    added base settings

---
+++ main.py
@@ -0,0 +1,47 @@
+import uvicorn
+
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+from settings.config import settings
+from core.database.db_helper import db_helper
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    yield
+    # shutdown
+    await db_helper.dispose()
+
+
+app = FastAPI(
+    default_response_class=ORJSONResponse,
+    lifespan=lifespan,
+)
+# main_app.include_router(
+#     api_router,
+# )
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-type", "Set-Cookie", "Access-Control-Allow-Headers", "Authorization"]
+)
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=True,
+    )
