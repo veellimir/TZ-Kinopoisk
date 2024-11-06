edd73effd9e5ec31f800e3d089c3ba9c58428bfb
vellimir <kkazancov@gmail.com>
2024-11-06 11:43:48 +0400

    added api router auth, users

--- app/authentication/dependencies/user_manager.py
+++ app/authentication/dependencies/user_manager.py
@@ -3,8 +3,7 @@ from typing import Annotated, TYPE_CHECKING
 from fastapi import Depends
 
 from app.authentication.user_manager import UserManager
-
-from .users import get_users_db
+from app.authentication.users import get_users_db
 
 if TYPE_CHECKING:
     from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
---
+++ app/authentication/messages.py
@@ -0,0 +1,43 @@
+from typing import Annotated
+
+from fastapi import APIRouter
+from fastapi import Depends
+
+from app.authentication.fastapiusers import (
+    current_active_user,
+    current_active_superuser,
+)
+from settings.config import settings
+from app.users.models import User
+from schemas.user import UserRead
+
+router = APIRouter(
+    prefix=settings.api.v1.messages,
+    tags=["Messages"],
+)
+
+
+@router.get("")
+def get_user_messages(
+    user: Annotated[
+        User,
+        Depends(current_active_user),
+    ],
+):
+    return {
+        "messages": ["m1", "m2", "m3"],
+        "user": UserRead.model_validate(user),
+    }
+
+
+@router.get("/secrets")
+def get_superuser_messages(
+    user: Annotated[
+        User,
+        Depends(current_active_superuser),
+    ],
+):
+    return {
+        "messages": ["secret-m1", "secret-m2", "secret-m3"],
+        "user": UserRead.model_validate(user),
+    }
